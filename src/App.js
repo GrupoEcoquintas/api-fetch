fe6435cade0dcebe1f673273a915d88bfaef3284
Luis R Zunniga S <admin@ecoquintas.com>
2023-03-06 10:31:42 -0600

    Agrego estilos usando MUI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import { TextField, Button } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import React, { useState } from "react";
+import { makeStyles } from '@mui/styles';
+
+
 const URL = 'http://api-rest.ecoquintas.net:3080/api/consulta-cliente/';
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px',
+    backgroundColor: '#f3f3f3',
+    borderRadius: '10px',
+    boxShadow: '0px 0px 10px rgba(0,0,0,0.2)',
+  },
+  button: {
+    marginTop: '20px',
+    backgroundColor: '#0077cc',
+    color: '#fff',
+    '&:hover': {
+      backgroundColor: '#0061a8',
+    },
+  },
+  error: {
+    color: 'red',
+    marginTop: '20px',
+    textAlign: 'center',
+  },
+});
+
+const CustomTextField = styled(TextField) ({
+  margin: '10px',
+  '& .MuiInputBase-input': {
+    color: 'green',
+    fontWeight: 'bold',
+  },
+  '& .MuiInputLabel-root': {
+    color: 'red',
+    fontWeight: 'bold',
+  },
+});
+
 
 function App() {
   // Definimos los estados que vamos a utilizar en nuestra aplicación con useState
@@ -8,6 +57,8 @@ function App() {
   const [cedula, setCedula] = useState("");
   const [fecha, setFecha] = useState("");
 
+  const classes = useStyles();
+  
   // Esta función se ejecuta cuando el usuario hace click en el botón 'Buscar'.
   const handleSubmit = async (e) => {
     e.preventDefault(); //Evita que el formulario se envíe por defecto.
@@ -25,7 +76,6 @@ function App() {
         setFecha(null);
       } else {
         // Si encontramos el cliente, seteamos la información del cliente y la fecha de la consulta y vaciamos el error.
-        setCliente(data.cliente);
         setCliente(data.cliente);
         setFecha(data.date);
         setError(null);
@@ -46,21 +96,30 @@ function App() {
 
   // Renderizamos nuestra aplicación.
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="cedula">Cédula:</label>
-        <input
-          type="text"
+    <div className={classes.container}>
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <CustomTextField
+          required
           id="cedula"
           name="cedula"
-          required
           value={cedula}
           onChange={handleCedulaChange}
+          variant="outlined"
+          label = "Cédula"
+          placeholder="Ingrese su cédula"
         />
-        <button type="submit">Buscar</button>
+        <Button className={classes.button} variant="contained" type="submit">Buscar</Button>
+        {error && <p className={classes.error}>{error}</p>}
       </form>
       {cliente && (
-        <div>
+        <div className='cliente-info' style={{ 
+          backgroundColor: '#f3f3f3',
+          padding: '20px',
+          borderRadius: '10px',
+          boxShadow: '0px 0px 10px rgba(0,0,0,0.2)',
+          marginTop: '20px',
+          textAlign: 'center',
+        }}>
           <h2>Información del cliente</h2>
           <p>
             Nombre: {cliente.nombre} {cliente.apellidos}
@@ -68,10 +127,10 @@ function App() {
           <p>Cédula: {cliente.cedula}</p>
           <p>Tipo de cliente: {cliente.tipo_cliente}</p>
           <p>Proyecto: {cliente.proyecto}</p>
+          {fecha && <p>Fecha de consulta: {fecha}</p>}
+          {error && <p>{error}</p>}
         </div>
       )}
-      {fecha && <p>Fecha de consulta: {fecha}</p>}
-      {error && <p>{error}</p>}
     </div>
   );
 }
